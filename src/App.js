aabb00d626949de8311667ed40edd36516cfc7a0
Alex S <justam4444@gmail.com>
2023-05-06 17:05:24 +0300

    Stikh stikh-interface added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from "./components/Header";
+import Stikh from './pages/stikh-interface';
 
 export default function App() {
   return (
     <div className="App">
-      <Header />
+      <Stikh />
     </div>
   );
 };
