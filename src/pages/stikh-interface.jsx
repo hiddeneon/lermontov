1b285a09f3139996661c034dd013c7f49bffe31b
Alex S <justam4444@gmail.com>
2023-05-07 20:50:18 +0300

    Set stikh-interface for performing only one vers

--- src/pages/stikh-interface.jsx
+++ src/pages/stikh-interface.jsx
@@ -8,10 +8,6 @@ export default function Stikh() {
             <div className="verse-container">
                 <h2>{poemsData[0].title}</h2>
                 <p>{poemsData[0].poem}</p>
-                <h2>{poemsData[1].title}</h2>
-                <p>{poemsData[1].poem}</p>
-                <h2>{poemsData[2].title}</h2>
-                <p>{poemsData[2].poem}</p>
             </div>
         </>
     );
