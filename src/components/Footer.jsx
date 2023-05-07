65efdb934d073ea96b8864f4606932f4cceca421
Alex S <justam4444@gmail.com>
2023-05-07 20:17:11 +0300

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p>2023</p>
+            <ul>
+                <li><a href="/">О проекте</a></li>
+            </ul>
+        </div>
+    )
+}
