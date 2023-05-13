389139d2a379e02466ea96f56d7eaf68889e4d2c
Alex S <justam4444@gmail.com>
2023-05-13 21:06:17 +0300

    Add carousel function

--- src/pages/stikh-interface.jsx
+++ src/pages/stikh-interface.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import poemsData from "../data/poemsData.json";
 import { useParams } from "react-router-dom";
+import useDocumentTitle from "../useDocumentTitle";
 
 export default function Stikh() {
 
     let { verseId } = useParams();
+    let verseElementNumber = poemsData.findIndex(({ URLTitle }) => URLTitle === verseId);
+    useDocumentTitle(poemsData[verseElementNumber].title);
 
     return (
         <div className="verse-content-body">
-            <h2>{poemsData[poemsData.findIndex(({ URLTitle }) => URLTitle === verseId)].title}</h2>
-            <div className="verse-container">
-                <p>{poemsData[poemsData.findIndex(({ URLTitle }) => URLTitle === verseId)].poem}</p>
-            </div>
+            
+                <h1>{poemsData[verseElementNumber].title}</h1>
+                <Link to={verseElementNumber > 0 ? "/stikhi/" + poemsData[verseElementNumber - 1].URLTitle : poemsData[0]}><button>{"<---"}</button></Link>
+                <div className="verse-container">
+                    <p>{poemsData[verseElementNumber].poem}</p>
+                </div>
+            <Link to={verseElementNumber < poemsData.length - 1 ? "/stikhi/" + poemsData[verseElementNumber + 1].URLTitle : poemsData[0]}><button>{"--->"}</button></Link>
         </div>
     );
 };
