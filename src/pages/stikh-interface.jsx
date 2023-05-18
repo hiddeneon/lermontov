3046b4400e070ca63ccf6898a955f84f772d8a7e
Alex S <justam4444@gmail.com>
2023-05-18 20:32:23 +0300

    Add arrows, edit stikh-interface flow

--- src/pages/stikh-interface.jsx
+++ src/pages/stikh-interface.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import poemsData from "../data/poemsData.json";
 import { useParams } from "react-router-dom";
 import useDocumentTitle from "../useDocumentTitle";
+import leftArrow from "../img/left-arrow.png";
+import rightArrow from "../img/right-arrow.png";
 
 export default function Stikh() {
 
@@ -12,13 +14,20 @@ export default function Stikh() {
 
     return (
         <div className="verse-content-body">
-            
-                <h1>{poemsData[verseElementNumber].title}</h1>
-                <Link to={verseElementNumber > 0 ? "/stikhi/" + poemsData[verseElementNumber - 1].URLTitle : poemsData[0]}><button>{"<---"}</button></Link>
-                <div className="verse-container">
-                    <p>{poemsData[verseElementNumber].poem}</p>
+            <h1>{poemsData[verseElementNumber].title}</h1>
+            <div className="verse-panel">
+                <div className="nav-button" id="left-arrow">
+                    <Link to={verseElementNumber > 0 ? "/stikhi/" + poemsData[verseElementNumber - 1].URLTitle : poemsData[0]}><button><img alt="предыдущий" src={leftArrow}  height="60px"/></button></Link>
                 </div>
-            <Link to={verseElementNumber < poemsData.length - 1 ? "/stikhi/" + poemsData[verseElementNumber + 1].URLTitle : poemsData[0]}><button>{"--->"}</button></Link>
+                <div className="verse-container">
+                    <div className="verse-body">
+                        <p>{poemsData[verseElementNumber].poem}</p>
+                    </div>
+                </div>
+                <div className="nav-button" id="right-arrow">
+                    <Link to={verseElementNumber < poemsData.length - 1 ? "/stikhi/" + poemsData[verseElementNumber + 1].URLTitle : poemsData[0]}><button><img alt="следующий" src={rightArrow}  height="60px"/></button></Link>
+                </div>
+            </div>
         </div>
     );
 };
