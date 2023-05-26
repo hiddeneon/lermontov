d7a20287c4e3867249302f1f9a1173422835cb55
Alex S <justam4444@gmail.com>
2023-05-26 20:56:45 +0300

    Footer's year component updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import Year from "./year";
 
 export default function Footer() {
+
+    const [color, setColor] = useState();
+
+    const colorBox = ["red", "yellow", "blue", "green", "orange", "pink", "chartreuse"];
+
+    let colorIndex = Math.floor(Math.random() * colorBox.length);
+
+    const yearColorToggle = () => {
+
+        while (colorBox[colorIndex] === color) { // excludes the chance to apply the same color twice in ar row
+            colorIndex = Math.floor(Math.random() * colorBox.length);
+        };
+
+        setColor(colorBox[colorIndex]); 
+    };
+
     return (
         <div className="footer">
             <div className="footer-content">
-                <div className="year">
-                    <p>2023</p>
-                </div>
+                <Year color={color} yearColorToggle={yearColorToggle} />
                 <div className="about">
                     <ul>
                         <li><a href="/">О проекте</a></li>
