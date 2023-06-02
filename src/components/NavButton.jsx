852c2a712a122b402b503821b7857f10ce7785d3
Alex S <justam4444@gmail.com>
2023-06-03 00:07:49 +0300

    Delete whitespace

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export default function NavButton({ verseElementNumber, poemsData, arrow }) {
     return (
         <div className="nav-button" id={arrow.class}>
-                <Link to={verseElementNumber > 0 ? "/stikhi/" + poemsData[arrow.class === "left-arrow" ? --verseElementNumber : ++verseElementNumber].URLTitle : poemsData[0]}><button><img alt={arrow.altSign} src={arrow.imgName}  height="60px"/></button></Link>
+                <Link to={verseElementNumber > 0 ? "/stikhi/" + poemsData[arrow.class === "left-arrow" ? --verseElementNumber : ++verseElementNumber].URLTitle : poemsData[0]}><button><img alt={arrow.altSign} src={arrow.imgName} height="60px"/></button></Link>
         </div>
     )
 }
